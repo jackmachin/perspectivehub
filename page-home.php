9e3bd1086a3ab0bb767bff9616b82c950aada2aa
jackmachin <jack.machin125@gmail.com>
2015-03-05 17:13:57 +0000

    Clearfixin'

--- page-home.php
+++ page-home.php
@@ -30,7 +30,7 @@
                 <img src="<?php echo get_template_directory_uri();?>/images/homelogos.jpg" alt="Homepage Logo Bar" title="Home Logos" />
             </div>
 
-            <div id="homenews">
+            <div id="homenews" class="cf">
                 <p class="fancy-bar-title gradient rounded">News feeds</p>
                 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("home-widgets") ) : ?>
                 <?php endif; ?>
