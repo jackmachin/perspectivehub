06720f75c619208fac8df24a4624075043b6209e
jackmachin <jack.machin125@gmail.com>
2015-03-10 10:22:15 +0000

    Cleaning up the js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
-jQuery(window).load(function(){
+jQuery(window).load(function () {
+    "use strict";
     
     // Fade in images so there isn't a color "pop" document load and then on window load
     //jQuery("ul.menu img").fadeIn(500);
@@ -41,43 +42,42 @@ jQuery(window).load(function(){
 	
 	// Alphabetical Index
 	jQuery('ul.letter_index > li').hide();
-	jQuery('ul.letter_index').find('li:first').css('display','block');
-	jQuery('ul.alphaIndex li a').click(function()
-	{
-	    var href=jQuery(this).attr('href');
-	    if(jQuery('ul.letter_index li'+href).is(':hidden'))
-	    {
-		closeIndex();
-		jQuery('ul.letter_index li'+href).show('fast');
+	jQuery('ul.letter_index').find('li:first').css('display', 'block');
+	jQuery('ul.alphaIndex li a').click(function () {
+	    var href = jQuery(this).attr('href');
+	    if (jQuery('ul.letter_index li' + href).is(':hidden')) {
+            closeIndex();
+
+            jQuery('ul.letter_index li' + href).show('fast');
 	    }
 	    return false;
 	});
 	
 	// Calendar workaround
-	jQuery('table.my-calendar-table a.trigger').click(function() {
+	jQuery('table.my-calendar-table a.trigger').click(function () {
 	    jQuery(this).parent().find('div.calendar-events').slideToggle('fast');
 	    return false;
-	});	
-	jQuery('a.mc-close').click(function() {
+	});
+	jQuery('a.mc-close').click(function () {
 	    jQuery(this).parent().parent().parent().parent().slideUp('fast');
 	    return false;
 	});
 	
 	// Footer menu CSS workaround
-	jQuery('footer div.menu-footer-menu-container ul').find('li:first').css('background','none');
+	jQuery('footer div.menu-footer-menu-container ul').find('li:first').css('background', 'none');
 	
 	// Header menu CSS workaround
 	//jQuery('ul#menu-main-menu > li:last').css('background','none').css('padding-right','0px');
 	
 	// RSS Blocks fancy styff
 	jQuery('aside.rsspageblock > ul:gt(1)').hide();
-	jQuery('aside.rsspageblock h3 a').click(function() {
+	jQuery('aside.rsspageblock h3 a').click(function () {
 	    jQuery(this).parent().parent().find('ul').slideToggle();
 	    return false;
 	});
 	
 	// RSS Logos fix
-	jQuery('aside.rsspageblock h3 img').attr('width','auto').attr('height','15px');
+	jQuery('aside.rsspageblock h3 img').attr('width', 'auto').attr('height', '15px');
 	
 	
 	// Homepage news ticker
@@ -92,30 +92,30 @@ jQuery(window).load(function(){
 	// LogOut Button/link
 	var logoutLink = jQuery('span#logoutButton').html();
 	jQuery('.menu-footer-menu-container').
-	append('<img src="/wp-content/themes/hub/images/presentations.jpg" id="footer-hub-logo" alt="" title="" /><br /><a href="'+logoutLink+'" class="logout_link">Logout</a>');
+        append('<img src="/wp-content/themes/hub/images/presentations.jpg" id="footer-hub-logo" alt="" title="" /><br /><a href="' + logoutLink + '" class="logout_link">Logout</a>');
 });
 
-function closeIndex()
-{
-    jQuery('ul.letter_index > li').each(function() {
-	if(jQuery(this).is(':visible'))
-	{
-	    jQuery(this).hide('fast');
-	}
+function closeIndex() {
+    "use strict";
+    jQuery('ul.letter_index > li').each(function () {
+        if (jQuery(this).is(':visible')) {
+            jQuery(this).hide('fast');
+        }
     });
 }
 	
 // Grayscale w canvas method
-function grayscale(src){
+function grayscale(src) {
+    "use strict";
 	var canvas = document.createElement('canvas');
 	var ctx = canvas.getContext('2d');
 	var imgObj = new Image();
 	imgObj.src = src;
 	canvas.width = imgObj.width;
-	canvas.height = imgObj.height; 
-	ctx.drawImage(imgObj, 0, 0); 
+	canvas.height = imgObj.height;
+	ctx.drawImage(imgObj, 0, 0);
 	var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
-	for(var y = 0; y < imgPixels.height; y++){
+	for (var y = 0; y < imgPixels.height; y++){
 	    for(var x = 0; x < imgPixels.width; x++){
 		var i = (y * 4) * imgPixels.width + x * 4;
 		var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
@@ -127,4 +127,3 @@ function grayscale(src){
 	ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
 	return canvas.toDataURL();
 }
-	
