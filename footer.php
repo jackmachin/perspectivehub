d847cc9be457979bcd3d4c562bd6619dc506e9c8
jackmachin <jack.machin125@gmail.com>
2015-03-10 09:21:17 +0000

    Remove site generator

--- footer.php
+++ footer.php
@@ -14,17 +14,13 @@
 
 	<footer id="colophon" role="contentinfo">
 
-			<?php
-				/* A sidebar in the footer? Yep. You can can customize
-				 * your footer with three columns of widgets.
-				 */
-				get_sidebar( 'footer' );
-			?>
+        <?php
+            /* A sidebar in the footer? Yep. You can can customize
+             * your footer with three columns of widgets.
+             */
+            get_sidebar( 'footer' );
+        ?>
 
-			<div id="site-generator">
-				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
-			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
