b6ae217a2fb82ef2e472ec7b55eb5953fc01183e
jackmachin <jack.machin125@gmail.com>
2015-03-12 10:33:41 +0000

    Don't ESC ATTR, break everything

--- searchform.php
+++ searchform.php
@@ -9,7 +9,7 @@
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<label>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( '<span class="screen-reader-text">Search the Hub</span>', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search :', 'label' ) ?>" />
+		<input type="search" class="search-field" placeholder="<?php echo ( '<span class="screen-reader-text">Search the Hub</span>', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search :', 'label' ) ?>" />
 	</label>
 	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
 </form>
