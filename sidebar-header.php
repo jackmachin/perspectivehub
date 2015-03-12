17099b3d96ce239715cbf31ee93820c70334d70c
jackmachin <jack.machin125@gmail.com>
2015-03-12 09:58:27 +0000

    Sidebar header

---
+++ sidebar-header.php
@@ -0,0 +1,10 @@
+<div id="sidebar-header" class="sidebar" role="complementary">
+
+    <?php if ( is_active_sidebar( 'sidebar-header' ) ) { ?>
+
+        <?php dynamic_sidebar( 'sidebar-header' ); ?>
+
+    <?php } ?>
+
+
+</div>
