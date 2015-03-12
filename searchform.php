ed93f0e1f0a54d9fef177bfeb074457e1d39bf50
jackmachin <jack.machin125@gmail.com>
2015-03-12 10:37:45 +0000

    Fixing search form

--- searchform.php
+++ searchform.php
@@ -8,8 +8,8 @@
  */
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-	<label>
-		<input type="search" class="search-field" placeholder="<?php echo '<span class="screen-reader-text">Search the Hub</span>' ;?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search :', 'label' ) ?>" />
-	</label>
-	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+ <label>
+  <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search the Hub', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search :', 'label' ) ?>" />
+ </label>
+ <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
 </form>
