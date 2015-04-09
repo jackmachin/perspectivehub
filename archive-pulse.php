66283637f39ddc2bf85595929c19dced684adece
jackmachin <jack.machin125@gmail.com>
2015-04-09 09:30:07 +0100

    Pulse

--- archive-pulse.php
+++ archive-pulse.php
@@ -14,49 +14,40 @@
 
 get_header(); ?>
 
-		<section id="primary">
-			<div id="content" role="main">
+    <div id="primary">
+        <div id="content" role="main">
 
-			<?php if ( have_posts() ) : ?>
+        <?php if ( have_posts() ) : ?>
 
-				<header class="page-header">
-					<h1 class="page-title">
-                        <?php post_type_archive_title(); ?>
-					</h1>
-				</header>
+            <header class="page-header">
+                <h1 class="page-title">
+                    <?php post_type_archive_title(); ?>
+                </h1>
+            </header>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+            <?php /* Start the Loop */ ?>
+            <?php while ( have_posts() ) : the_post(); ?>
 
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <h1 class="beat-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+                      </header><!-- .entry-header -->
+
+                    <div class="entry-content">
+                        <?php the_contennt() ;?>
+                    </div><!-- .entry-content -->
 
 				<?php endwhile; ?>
+               </article><!-- #post-<?php the_ID(); ?> -->
 
-				<?php twentyeleven_content_nav( 'nav-below' ); ?>
+            <?php endwhile; ?>
 
 			<?php else : ?>
 
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
+        <?php endif; ?>
 
-					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-
-			</div><!-- #content -->
-		</section><!-- #primary -->
+        </div><!-- #content -->
+    </section><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
