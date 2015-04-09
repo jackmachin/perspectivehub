24d2383f0a777a2a55d73715d8276e7271f19d7f
jackmachin <jack.machin125@gmail.com>
2015-04-09 10:03:19 +0100

    SUTFF

--- sidebar.php
+++ sidebar.php
@@ -6,11 +6,6 @@
  * @subpackage Twenty_Eleven
  * @since Twenty Eleven 1.0
  */
-
-$options = twentyeleven_get_theme_options();
-$current_layout = $options['theme_layout'];
-
-if ( 'content' != $current_layout ) :
 ?>
 		<div id="secondary" class="widget-area" role="complementary">
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
