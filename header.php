d7f0b3343749a7e4d2f1fc16ef3f84b032467792
jackmachin <jack.machin125@gmail.com>
2015-04-10 10:41:01 +0100

    UBSHFG

--- header.php
+++ header.php
@@ -50,10 +50,6 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_stylesheet_directory_uri(); ?>/style.min.css" />
 
-<!--[if IE]>
-    <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_stylesheet_directory_uri(); ?>/ie.css" />
-<![endif]-->
-
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
