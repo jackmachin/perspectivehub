17fef8da2edaa983b15c4c700af5587ebcb6b206
jackmachin <jack.machin125@gmail.com>
2015-04-22 10:25:05 +0100

    Single pulse

--- single-pulse.php
+++ single-pulse.php
@@ -28,7 +28,7 @@ get_header(); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<h1 class="entry-title pulse-title"><?php the_title(); ?></h1>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
