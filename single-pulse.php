81bd0353e5f68a1dd977ea6921c0e12e09b42306
jackmachin <jack.machin125@gmail.com>
2015-08-06 11:21:04 +0100

    Page nav move

--- single-pulse.php
+++ single-pulse.php
@@ -15,14 +15,6 @@ get_header(); ?>
 
             <?php while ( have_posts() ) : the_post(); ?>
 
-                    <nav id="nav-single">
-
-                        <?php /*<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>*/ ?>
-
-                        <span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> Previous', 'twentyeleven' ) ); ?></span>
-						<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></span>
-                    </nav>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title pulse-title"><?php the_title(); ?></h1>
@@ -42,6 +34,14 @@ get_header(); ?>
 	<footer class="entry-meta">
 
 	</footer><!-- .entry-meta -->
+
+        <nav id="nav-single">
+
+                <?php /*<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentyeleven' ); ?></h3>*/ ?>
+
+                        <span class="nav-previous"><?php previous_post_link( '%link', __( '<span class="meta-nav">&larr;</span> Previous', 'twentyeleven' ) ); ?></span>
+						<span class="nav-next"><?php next_post_link( '%link', __( 'Next <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></span>
+                    </nav>
 </article><!-- #post-<?php the_ID(); ?> -->
 
 				<?php endwhile; // end of the loop. ?>
