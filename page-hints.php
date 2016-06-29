8ead59ef84bcfa744bba4c305f8cfcb95f4dc556
Jack Machin <jack.machin125@gmail.com>
2016-06-29 15:10:31 +0100

    Will this work?

--- page-hints.php
+++ page-hints.php
@@ -19,15 +19,39 @@ get_header(); ?>
                         <p><img class="alignleft wp-image-2605 size-full" title="hints-n-tips" src="http://www.perspectivehub.co.uk/wp-content/uploads/2013/08/hints-n-tips.png" alt="" width="138" height="200" /><strong>If you have any queries regarding the hints and tips please do not hesitate to contact Richard Heywood, Business Monitoring Manager, on 0161 244 9759 or at <a href="mailto:richard.heywood@pfgl.co.uk">richard.heywood@pfgl.co.uk</a>.</strong></p>
                     </div>
                     <div class="post-container">
-
-                        <div class="article-sidebar">
-
+                              <div class="article-sidebar">
+      <h2><strong>Hints &amp; Tips</strong></h2>
                             <ul class="quick-links">
-                                <h2><strong>Hints &amp; Tips</strong></h2>
+<?php $args = array(
+	'posts_per_page'   => 50,
+	'offset'           => 0,
+	'category'         => '',
+	'category_name'    => '',
+	'orderby'          => 'date',
+	'order'            => 'DESC',
+	'include'          => '',
+	'exclude'          => '',
+	'meta_key'         => '',
+	'meta_value'       => '',
+	'post_type'        => 'file-review-hint',
+	'post_mime_type'   => '',
+	'post_parent'      => '',
+	'author'	   => '',
+	'author_name'	   => '',
+	'post_status'      => 'publish',
+	'suppress_filters' => true
+);
+
+$postslist = get_posts( $args );
+foreach ( $postslist as $post ) :
+  setup_postdata( $post ); ?>
 
                                 <li>
-                                    Hold
+                                    <?php the_title();?>
                                 </li>
+<?php
+endforeach;
+wp_reset_postdata();?>
                             </ul>
                         </div>
 
