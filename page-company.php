1e630d2556bf2d34b683f17a5a7092c4a1fa8cf3
Jack Machin <jack.machin125@gmail.com>
2016-08-25 15:12:42 +0100

    Issues

--- page-company.php
+++ page-company.php
@@ -10,7 +10,23 @@ get_header(); ?>
         <div id="content" role="main">
             <?php the_post(); ?>
             <?php get_template_part( 'content', 'page' ); ?>
-               <div class="company-content">
+                <div class="company-map">
+                    <?php
+                        $args = array( 'post_type' => 'companies', 'posts_per_page' => 1000 );
+                        $loop = new WP_Query( $args );
+                    ?>
+                    <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                        <?php if (get_field ('map')) { ?>
+                            <a id="<?php the_field('id');?>" onclick="selectThis('<?php the_field('id');?>')" href="javascript:void(0);" class="bullet preview" style="top:<?php the_field('y-coord');?>px;left:<?php the_field('x-coord');?>px;"><?php the_title();?>
+                                <img class="map-logo" id="map-logo-<?php the_field ('id');?>" src="<?php the_field('company-logo');?>" alt="<?php the_field('id');?>-Logo" width="220" height="55">
+                            </a>
+                        <?php } ?>
+                    <?php endwhile; ?>
+                </div>
+
+                <?php wp_reset_query(); // RESETTING THE QUERY TO SET IT TO THE SAME THING SEEMS SMART ?>
+
+                <div class="company-content">
 				    <?php
                         $args = array( 'post_type' => 'companies', 'posts_per_page' => 1000 );
                         $loop = new WP_Query( $args );
@@ -45,23 +61,6 @@ get_header(); ?>
 
                     <?php endwhile; ?>
                 </div>
-
-                <?php wp_reset_query(); // RESETTING THE QUERY TO SET IT TO THE SAME THING SEEMS SMART ?>
-
-                <div class="company-map">
-                    <?php
-                        $args = array( 'post_type' => 'companies', 'posts_per_page' => 1000 );
-                        $loop = new WP_Query( $args );
-                    ?>
-                    <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-                        <?php if (get_field ('map')) { ?>
-                            <a id="<?php the_field('id');?>" onclick="selectThis('<?php the_field('id');?>')" href="javascript:void(0);" class="bullet preview" style="top:<?php the_field('y-coord');?>px;left:<?php the_field('x-coord');?>px;"><?php the_title();?>
-                                <img class="map-logo" id="map-logo-<?php the_field ('id');?>" src="<?php the_field('company-logo');?>" alt="<?php the_field('id');?>-Logo" width="220" height="55">
-                            </a>
-                        <?php } ?>
-                    <?php endwhile; ?>
-                </div>
-
         </div><!-- #content -->
     </div><!-- #primary -->
 <?php get_footer(); ?>
