a71662bf8298a75e712e9abdaf9b299049098044
Jack Machin <jack.machin125@gmail.com>
2016-08-25 14:04:43 +0100

    Company

--- page-company.php
+++ page-company.php
@@ -10,26 +10,16 @@ get_header(); ?>
         <div id="content" role="main">
             <?php the_post(); ?>
             <?php get_template_part( 'content', 'page' ); ?>
+                <div class="ifa-content">
+				<?php   $args = array( 'post_type' => 'companies', 'posts_per_page' => 1000 );
+                        $loop = new WP_Query( $args );
 
-				<?php
-				    $post_type='page';
-				    $special_posts=array('companies');
-				    if(in_array($post->post_name,$special_posts)) {
-					   $post_type=$post->post_name;
-					?>
-                    <?php $loop = new WP_Query( array(
-                        'post_type' => 'companies',
-                        'posts_per_page' => 1000 )
-                    ); ?>
-                    <div class="ifa-map">
-					   <?php if($loop->have_posts()): ?>
+                        while ( $loop->have_posts() ) : $loop->the_post();
+                            the_title(); ?>
+                            <img class="company-logo" src="<?php the_field('company-logo');?>">
 
-                            <a id="<?php the_field('id');?>" onclick="selectThis('<?php the_field('id');?>')" href="javascript:void(0);" class="bullet preview"><?php the_title();?>
-                                <img class="map-logo" id="map-logo-ad" src="<?php the_field('company-logo');?>" alt="<?php the_title();?>-Logo" width="220" height="55" style="top:<?php the_field('y-coord');?>;left:<?php the_field('x-coord');?>;">
-                            </a>
-                        <?php endif; ?>
-                    </div><!--ifa-map-->
-                    <?php } ?>
-			</div><!-- #content -->
-		</div><!-- #primary -->
+                        <?php endwhile; ?>
+                </div>
+        </div><!-- #content -->
+    </div><!-- #primary -->
 <?php get_footer(); ?>
