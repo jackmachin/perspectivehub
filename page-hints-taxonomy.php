4578b0fa48c2c5d1a890d23d5b348fa35f98c401
Jack Machin <jack.machin125@gmail.com>
2017-02-23 15:15:07 +0000

    Try this

---
+++ page-hints-taxonomy.php
@@ -0,0 +1,76 @@
+<?php
+/*
+* Template Name: Hints Taxonomy
+*/
+
+get_header(); ?>
+
+    <a name="top"></a>
+        <div id="primary">
+            <div id="content" role="main">
+			    <?php the_post(); ?>
+
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <h1 class="entry-title"><?php the_title(); ?></h1>
+                    </header><!-- .entry-header -->
+
+                    <div class="entry-content cf">
+                        <p><img class="alignleft wp-image-2605 size-full" title="hints-n-tips" src="http://www.perspectivehub.co.uk/wp-content/uploads/2013/08/hints-n-tips.png" alt="" width="138" height="200" /><strong>If you have any queries regarding the hints and tips please do not hesitate to contact Richard Heywood, Business Monitoring Manager, on 0161 244 9759 or at <a href="mailto:richard.heywood@pfgl.co.uk">richard.heywood@pfgl.co.uk</a>.</strong></p>
+                    </div>
+                    <div class="post-container">
+                              <div class="entry-content article-sidebar">
+      <h2><strong>Hints &amp; Tips</strong></h2>
+                            <ul class="quick-links">
+<?php $args = array(
+	'posts_per_page'   => -1,
+	'orderby'          => 'date',
+	'order'            => 'DESC',
+	'post_type'        => 'file-review-hint',
+	'post_status'      => 'publish',
+    'taxonomy'      => 'legislation',
+);
+
+$postslist = get_posts( $args );
+foreach ( $postslist as $post ) :
+  setup_postdata( $post ); ?>
+
+                                <li>
+                                    <a href="#<?php the_ID();?>"><?php the_title();?></a>
+                                </li>
+<?php
+endforeach;
+wp_reset_postdata();?>
+                            </ul>
+                        </div>
+
+
+<?php $args = array(
+	'posts_per_page'   => -1,
+	'orderby'          => 'date',
+	'order'            => 'DESC',
+	'post_type'        => 'file-review-hint',
+	'post_status'      => 'publish',
+    'taxonomy'      => 'legislation',
+);
+
+$postslist = get_posts( $args );
+foreach ( $postslist as $post ) :
+  setup_postdata( $post ); ?>
+
+                        <section class="article entry-content">
+                            <h2 id="<?php the_ID();?>" class="file-review-title"><strong><?php the_title();?> -  <?php the_time( get_option( 'date_format' ) ); ?></strong></h2>
+                            <div class="file-review-content">
+                                <?php the_content();?>
+                                <p><a href="#top">Back to top...</a></p>
+                            </div>
+                        </section>
+<?php
+endforeach;
+wp_reset_postdata();?>
+                    </div><!-- .entry-content -->
+                </article><!-- #post-<?php the_ID(); ?> -->
+			</div><!-- #content -->
+		</div><!-- #primary -->
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
