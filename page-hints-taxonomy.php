d1000109ec052ed669182df9ac6655856e71abec
Jack Machin <jack.machin125@gmail.com>
2017-02-23 15:22:07 +0000

    Get the title

--- page-hints-taxonomy.php
+++ page-hints-taxonomy.php
@@ -3,28 +3,30 @@
 * Template Name: Hints Taxonomy
 */
 
-get_header(); ?>
+get_header();
+?>
 
-    <a name="top"></a>
-        <div id="primary">
-            <div id="content" role="main">
-			    <?php the_post(); ?>
+<a name="top"></a>
+    <div id="primary">
+        <div id="content" role="main">
+            <?php the_post(); ?>
 
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header">
-                        <h1 class="entry-title"><?php the_title(); ?></h1>
-                    </header><!-- .entry-header -->
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
+                </header><!-- .entry-header -->
 
-                    <div class="entry-content cf">
-                        <p><img class="alignleft wp-image-2605 size-full" title="hints-n-tips" src="http://www.perspectivehub.co.uk/wp-content/uploads/2013/08/hints-n-tips.png" alt="" width="138" height="200" /><strong>If you have any queries regarding the hints and tips please do not hesitate to contact Richard Heywood, Business Monitoring Manager, on 0161 244 9759 or at <a href="mailto:richard.heywood@pfgl.co.uk">richard.heywood@pfgl.co.uk</a>.</strong></p>
-                    </div>
-                    <div class="post-container">
-                              <div class="entry-content article-sidebar">
-      <h2><strong>Hints &amp; Tips</strong></h2>
-                            <ul class="quick-links">
+                <div class="entry-content cf">
+                    <p><img class="alignleft wp-image-2605 size-full" title="hints-n-tips" src="http://www.perspectivehub.co.uk/wp-content/uploads/2013/08/hints-n-tips.png" alt="" width="138" height="200" /><strong>If you have any queries regarding the hints and tips please do not hesitate to contact Richard Heywood, Business Monitoring Manager, on 0161 244 9759 or at <a href="mailto:richard.heywood@pfgl.co.uk">richard.heywood@pfgl.co.uk</a>.</strong></p>
+                </div>
+
+                <div class="post-container">
+                    <div class="entry-content article-sidebar">
+                        <h2><strong>Hints &amp; Tips</strong></h2>
+                    <ul class="quick-links">
 <?php
 
-    $tax = the_title();
+    $tax = get_the_title();
 
     $args = array(
 	'posts_per_page'   => -1,
