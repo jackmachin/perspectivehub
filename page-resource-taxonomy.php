3e8f510ab16beb94e6d55946171da49d0ceb5290
Jack Machin <jack.machin125@gmail.com>
2017-03-30 11:36:55 +0100

    Values

--- page-resource-taxonomy.php
+++ page-resource-taxonomy.php
@@ -47,7 +47,7 @@ get_header();
 
                         <h2 id="<?php the_ID();?>" class="resource-title">
 
-                            <?php if (get_field('file_format') == 'PDF' ) { ?>
+                            <?php if (get_field('file_format') == "PDF" ) { ?>
                                 <img src="" class="icon" alt="PDF Icon" width="30" height="30">
                             <?php } else {} ?>
 
